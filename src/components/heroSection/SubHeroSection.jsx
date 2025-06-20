174795f5f8ab3164e2b43d8f8b2b24b2f594d2c4
navonsanjuni <navonsanjuni178@gmail.com>
2025-06-20 14:02:33 +0530

    Created about me section

--- src/components/heroSection/SubHeroSection.jsx
+++ src/components/heroSection/SubHeroSection.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SubHeroSection = () => {
   return (
-    <div>SubHeroSection</div>
+    <div className='w-full border-lightGrey text-lightGrey border-y flex justify-around uppercase xl:text-4xl md:text-2xl sm:text-xl items-center py-8 gap-4 bg-brown'>
+      <p className='md:block sm:hidden'>Fast Learner</p>
+      <p className='md:block sm:hidden'>Team Work</p>
+      <p>Details Master</p>
+    </div>
   )
 }
 
